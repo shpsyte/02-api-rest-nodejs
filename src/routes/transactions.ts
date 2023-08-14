ab84c77fc0ac2f5aa86f55e2c60c849cfdd94e90
luiz <jose.iscosistemas@gmail.com>
2023-08-14 16:20:00 -0700

    criando o primeiro post de dados

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,25 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
-
+import z from 'zod'
+import crypto from 'node:crypto'
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/transactions', async () => {
-    const transactions = await knex('transactions').select('*')
+  app.post('/', async (req, res) => {
+    const schema = z.object({
+      amount: z.number(),
+      title: z.string(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    const { amount, title, type } = schema.parse(req.body)
+
+    await knex('transactions')
+      .insert({
+        id: crypto.randomUUID(),
+        amount: type === 'debit' ? -amount : amount,
+        title,
+      })
+      .returning('*')
+
+    return res.code(201).send()
   })
 }
